e7632c7034e43f63b37777f2cdaadb23fe4bacbc
robcabobb <robcabobb@gmail.com>
2022-02-14 13:04:09 -0500

    add front end code and routes to pages

--- server.js
+++ server.js
@@ -12,6 +12,9 @@ app.use(express.urlencoded({extended: true}))
 // parse incoming JSON data
 app.use(express.json())
 
+app.use(express.static('public'));
+
+
 function filterByQuery(query, animalsArray) {
   let personalityTraitsArray = []
 
@@ -104,6 +107,22 @@ app.get('/api/animals/:id', (req, res) => {
     res.send(404)
   }
 })
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/animals', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+
+app.get('/zookeepers', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
 
 app.listen(PORT, () => {
 	console.log(`API server now on port 3001!`);
