ff093f29d84e9c28bba823cc7778feb0f12f84ae
robcabobb <robcabobb@gmail.com>
2022-05-17 21:59:57 -0400

    fixed code

--- __tests__/zookeepers.test.js
+++ __tests__/zookeepers.test.js
@@ -4,73 +4,12 @@ const {
   findById,
   createNewZookeeper,
   validateZookeeper
-} = require('../lib/zookeepers.js');const fs = require("fs");
-const path = require("path");
-
-function filterByQuery(query, zookeepers) {
-  let filteredResults = zookeepers;
-  if (query.age) {
-    filteredResults = filteredResults.filter(
-      // Since our form data will be coming in as strings, and our JSON is storing
-      // age as a number, we must convert the query string to a number to
-      // perform a comparison:
-      (zookeeper) => zookeeper.age === Number(query.age)
-    );
-  }
-  if (query.favoriteAnimal) {
-    filteredResults = filteredResults.filter(
-      (zookeeper) => zookeeper.favoriteAnimal === query.favoriteAnimal
-    );
-  }
-  if (query.name) {
-    filteredResults = filteredResults.filter(
-      (zookeeper) => zookeeper.name === query.name
-    );
-  }
-  return filteredResults;
-}
-
-function findById(id, zookeepers) {
-  const result = zookeepers.filter((zookeeper) => zookeeper.id === id)[0];
-  return result;
-}
-
-function createNewZookeeper(body, zookeepers) {
-  const zookeeper = body;
-  zookeepers.push(zookeeper);
-  fs.writeFileSync(
-    path.join(__dirname, "../data/zookeepers.json"),
-    JSON.stringify({ zookeepers }, null, 2)
-  );
-  return zookeeper;
-}
-
-function validateZookeeper(zookeeper) {
-  if (!zookeeper.name || typeof zookeeper.name !== "string") {
-    return false;
-  }
-  if (!zookeeper.age || typeof zookeeper.age !== "number") {
-    return false;
-  }
-  if (
-    !zookeeper.favoriteAnimal ||
-    typeof zookeeper.favoriteAnimal !== "string"
-  ) {
-    return false;
-  }
-  return true;
-}
-
-module.exports = {
-  filterByQuery,
-  findById,
-  createNewZookeeper,
-  validateZookeeper,
-};
+} = require('../lib/zookeepers.js');
+const { zookeepers } = require('../data/zookeepers');
 
 jest.mock('fs');
-test('creates an zookeeper object', () => {
-  const zookeeper = createNewZookeeper({ name: 'Darlene', id: 'jhgdja3ng2' }, []);
+test('creates a zookeeper object', () => {
+  const zookeeper = createNewZookeeper({ name: 'Darlene', id: 'jhgdja3ng2' }, zookeepers);
 
   expect(zookeeper.name).toBe('Darlene');
   expect(zookeeper.id).toBe('jhgdja3ng2');
--- routes/apiRoutes/animalRoutes.js
+++ routes/apiRoutes/animalRoutes.js
@@ -3,18 +3,26 @@ const { filterByQuery, findById, createNewAnimal, validateAnimal } = require('..
 const { animals } = require('../../data/animals');
 
 router.get('/animals', (req, res) => {
-  let results = animals
+  let results = animals;
   if (req.query) {
-    results = filterByQuery(req.query, results)
+    results = filterByQuery(req.query, results);
   }
   res.json(results);
-})
+});
+
+router.get('/animals/:id', (req, res) => {
+  const result = findById(req.params.id, animals);
+  if (result) {
+    res.json(result);
+  } else {
+    res.send(404);
+  }
+});
 
 router.post('/animals', (req, res) => {
   // set id based on what the next index of the array will be
   req.body.id = animals.length.toString();
 
-  // if any data in req.body is incorrect, send 400 error back
   if (!validateAnimal(req.body)) {
     res.status(400).send('The animal is not properly formatted.');
   } else {
@@ -23,14 +31,4 @@ router.post('/animals', (req, res) => {
   }
 });
 
-
-router.get('/animals/:id', (req, res) => {
-  const result = findById(req.params.id, animals)
-  if (result) {
-    res.json(result)
-  } else {
-    res.send(404)
-  }
-})
-
-module.exports  = router;
+module.exports = router;
